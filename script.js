180215ff7e3a2b7b1fbe23a65e7a58ec29a38252
facebookcomw05-sudo <facebook.com.w05@gmail.com>
2026-04-23 14:29:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // ==================== إعدادات Firebase (استبدلها ببياناتك) ====================
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
